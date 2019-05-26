683d3787d973c1239715329c24142a88b9aa5a9f
WikiWikiWiki <goyo566@gmail.com>
2019-05-26 19:19:12 +0200

    Fixed merge script for both classes.

--- data/student-merge.R
+++ data/student-merge.R
@@ -1,5 +1,20 @@
-d1=read.table("student-mat.csv",sep=";",header=TRUE)
-d2=read.table("student-por.csv",sep=";",header=TRUE)
+library(Rcmdr, quietly = TRUE)
 
-d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
-print(nrow(d3)) # 382 students
+# Change depending on your working directory
+working_directory <- "F:/Drive/Máster Data Science/Tipología y ciclo de la vida de los datos/PRAC2/student-alcohol-consumption/data"
+setwd(working_directory)
+
+
+students_mat <- read.table("student-mat.csv", header=TRUE, sep=",", na.strings="NA", dec=".", strip.white=TRUE)
+students_por <- read.table("student-por.csv", header=TRUE, sep=",", na.strings="NA", dec=".", strip.white=TRUE)
+
+
+students <- merge(students_mat, students_por, by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
+
+print(nrow(students))
+
+summary(students_mat)
+summary(students_por)
+summary(students)
+
+write.table(students, "students.csv", sep=",", col.names=TRUE, row.names=TRUE, quote=TRUE, na="NA")
